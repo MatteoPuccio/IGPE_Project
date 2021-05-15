014435e3699bf0b52b0310a3bb5c1b64af3fb3d9
Giacomo Galavotti <gciaooo@gmail.com>
2021-05-15 17:44:57 +0200

    Ampliamento classe Gun e Bullet

--- IGPE_Project/core/src/com/mygdx/game/model/Gun.java
+++ IGPE_Project/core/src/com/mygdx/game/model/Gun.java
@@ -2,11 +2,28 @@ package com.mygdx.game.model;
 
 import java.util.ArrayList;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Gun {
 	
 	int damage;
 	int ammunition;
 	float range;
+	Vector2 aim;
 	ArrayList<Bullet> bullets;
+	ArrayList<Bullet> released;
 	
+	void reload() {
+		bullets.clear();
+		for (int i = 0; i < ammunition; i++) {
+			bullets.add(new Bullet());
+		}
+	}
+	
+	void shoot() {
+		released.add(bullets.get(0));
+		bullets.remove(0);
+		released.get(released.size()-1).x = aim.x;
+		released.get(released.size()-1).y = aim.y;
+	}
 }
