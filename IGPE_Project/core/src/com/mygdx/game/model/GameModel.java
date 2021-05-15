a90cdffea46e0c2f2b8fa9a0aff21600895e2ac9
Riccardo <Riccardo@DESKTOP-IQ7PUPM>
2021-05-15 18:08:37 +0200

    Creata una prova grafica per verificare il funzionamento dei metodi

--- IGPE_Project/core/src/com/mygdx/game/model/GameModel.java
+++ IGPE_Project/core/src/com/mygdx/game/model/GameModel.java
@@ -1,5 +1,24 @@
 package com.mygdx.game.model;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class GameModel {
 	
+	private Character character;
+	
+	private static GameModel gameModel = null;
+	
+	private GameModel() {
+		character = new Character(new Vector2(0,0));
+	}
+	
+	public static GameModel getInstance() {
+		if(gameModel == null)
+			gameModel = new GameModel();
+		return gameModel;
+	}
+	
+	public Character getCharacter() {
+		return character;
+	}
 }
