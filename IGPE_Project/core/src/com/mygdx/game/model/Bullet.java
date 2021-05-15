6b38d789a9ba03a3936f9f60d447fd8e2cadefb6
Giacomo Galavotti <gciaooo@gmail.com>
2021-05-15 18:12:18 +0200

    update Bullet e Gun

--- IGPE_Project/core/src/com/mygdx/game/model/Bullet.java
+++ IGPE_Project/core/src/com/mygdx/game/model/Bullet.java
@@ -3,5 +3,8 @@ package com.mygdx.game.model;
 import com.badlogic.gdx.math.Vector2;
 
 public class Bullet extends Vector2{
-	
+	public Bullet(Vector2 aim) {
+		this.x = aim.x;
+		this.y = aim.y;
+	}
 }
