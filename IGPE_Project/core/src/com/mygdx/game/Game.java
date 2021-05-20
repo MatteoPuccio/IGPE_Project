1deff3ec12cf2544136d0682cb41826926af7a45
Riccardo <Riccardo@DESKTOP-IQ7PUPM>
2021-05-20 17:40:24 +0200

    Aggiunta la classe Animation

--- IGPE_Project/core/src/com/mygdx/game/Game.java
+++ IGPE_Project/core/src/com/mygdx/game/Game.java
@@ -24,7 +24,7 @@ public class Game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);	
 		
-		controller.getView().render();
+		controller.getView().render(Gdx.graphics.getDeltaTime());
 	}
 
 	@Override
