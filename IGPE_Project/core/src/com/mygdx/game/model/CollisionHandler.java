47d3b24f5efa82007a7e990d3ce503249deebf1f
Riccardo <Riccardo@DESKTOP-IQ7PUPM>
2021-06-12 18:41:05 +0200

    Eliminata classe duplicata

--- IGPE_Project/core/src/com/mygdx/game/model/CollisionHandler.java
+++ IGPE_Project/core/src/com/mygdx/game/model/CollisionHandler.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
+import com.mygdx.game.model.entities.EnemiesHandler;
 
 public class CollisionHandler implements ContactListener { 
 
