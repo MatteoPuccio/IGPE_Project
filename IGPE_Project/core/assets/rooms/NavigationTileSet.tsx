232a5a9f2f12d7f83f792dcbcd3848ef0c4ed0e2
Riccardo <Riccardo@DESKTOP-IQ7PUPM>
2021-06-21 14:59:20 +0200

    Aggiunto tileset per il pathfinding

---
+++ IGPE_Project/core/assets/rooms/NavigationTileSet.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="NavigationTileSet" tilewidth="16" tileheight="16" tilecount="2" columns="2">
+ <image source="NavigationTileSet.png" width="32" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="walkable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="walkable" value="0"/>
+  </properties>
+ </tile>
+</tileset>
