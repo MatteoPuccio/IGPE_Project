172920ac2e6f9b10a4f3e16d4ff167f16d07f6ee
Giacomo Galavotti <gciaooo@gmail.com>
2021-06-24 18:58:30 +0200

    Rinomine livelli + aggiunta file mancante

---
+++ IGPE_Project/core/assets/rooms/spawnTileset.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="spawnTileset" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="../../../../../../Desktop/spawnTileset.png" width="64" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="spawnpoints" value="n"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="spawnpoints" value="e"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="spawnpoints" value="w"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="spawnpoints" value="s"/>
+  </properties>
+ </tile>
+</tileset>
