6fbb703e65465fc5489b852c4a5bcfbbf1b786ac
matte <matte@DESKTOP-PHG5H5M.homenet.telecomitalia.it>
2021-06-27 18:17:43 +0200

    rimosso import non utilizzato

--- IGPE_Project/core/src/com/mygdx/game/model/level/RoomHandler.java
+++ IGPE_Project/core/src/com/mygdx/game/model/level/RoomHandler.java
@@ -1,6 +1,5 @@
 package com.mygdx.game.model.level;
 
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.controller.ParticleHandler;
 import com.mygdx.game.controller.SoundHandler;
